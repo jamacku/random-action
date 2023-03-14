64555c86d766a64cea8fe1efac1e8bbab910e3ad
Jan Macku <jamacku@redhat.com>
2023-03-14 17:35:12 +0100

    Initial commit

---
+++ dist/action.js
@@ -0,0 +1,42 @@
+import { getInput, summary } from '@actions/core';
+import { events } from './events';
+const action = (probot) => {
+    probot.on(events.workflow_run, async (context) => {
+        // TODO: get config
+        // TODO: use zod to check if input is number string and transform to number
+        const prNumber = getInput('pr', { required: true });
+        const { status, data } = await context.octokit.pulls.listCommits(context.repo({ pull_number: +prNumber }));
+        if (status !== 200) {
+            throw new Error('Could not get commits');
+        }
+        const lastCommit = data[data.length - 1];
+        // TODO: set statuses as pending
+        await context.octokit.repos.createCommitStatus(context.repo({
+            state: 'error',
+            sha: lastCommit.sha,
+            description: 'description',
+            target_url: 'url',
+        }));
+        // TODO: update statuses as success or failure
+        summary
+            .addHeading('Summary')
+            .addBreak()
+            .addTable([
+            [
+                { data: 'File', header: true },
+                { data: 'Result', header: true },
+            ],
+            ['foo.js', 'Pass '],
+            ['bar.js', 'Fail '],
+            ['test.js', 'Pass '],
+        ])
+            .addLink('View staging deployment!', 'https://github.com')
+            .write();
+        await context.octokit.issues.createComment(context.repo({
+            issue_number: +prNumber,
+            body: 'Hello World!',
+        }));
+    });
+};
+export default action;
+//# sourceMappingURL=action.js.map
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { setFailed } from '@actions/core';
+import run from '@probot/adapter-github-actions';
+
+import '@total-typescript/ts-reset';
+
+import action from './action';
+
+try {
+  await run.run(action);
+} catch (error) {
+  error instanceof Error
+    ? setFailed(error.message)
+    : setFailed(error as string);
+}
